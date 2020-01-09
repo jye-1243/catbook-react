ef7a5f7f1bee10b3f2d1d9ae8e6ca67a8efa5288
Steven Luscher <steveluscher@fb.com>
2020-01-08 21:37:27 -0800

    Add the Comment, Story, and User types to the GraphQL schema from the previous step. Create a number of root fields in the GraphQL schema that can be used to get a comment, story, or user, as well as all the stories as a list. Create resolver functions to actually get the data from MongoDB for each one of these fields. To see this in action, start the server (npm start) and visit http://localhost:3000/graphql.

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,7 +1,47 @@
+const Comment = require("../models/comment");
+const Story = require("../models/story");
+const User = require("../models/user");
+
 module.exports = {
+  Comment: {
+    creator(comment) {
+      // Some docs in the class' database have bad ids. Return a 'fake' user.
+      if (comment.creator_id == null || comment.creator_id === "testid") {
+        return {
+          id: comment.creator_id,
+          name: comment.creator_name,
+        };
+      }
+      return User.findById(comment.creator_id);
+    },
+  },
+  Story: {
+    comments(story) {
+      return Comment.find({ parent: story.id });
+    },
+    creator(story, _args, _req, context) {
+      // Some docs in the class' database have bad ids. Return a 'fake' user.
+      if (story.creator_id == null || story.creator_id === "testid") {
+        return {
+          id: story.creator_id,
+          name: story.creator_name,
+        };
+      }
+      return User.findById(story.creator_id);
+    },
+  },
   Query: {
-    hello() {
-      return "world";
+    comment(_, args) {
+      return Comment.findById(args.id);
+    },
+    story(_, args) {
+      return Story.findById(args.id);
+    },
+    user(_, args) {
+      return User.findById(args.id);
+    },
+    stories() {
+      return Story.find({});
     },
   },
 };
